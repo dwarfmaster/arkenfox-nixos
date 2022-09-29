c92789885c38936f314b0cdf20c65eac984b103a
DwarfMaster <luc@dwarfmaster.net>
2022-09-29 23:40:57 +0200

    Add support for firefox 105.0

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
       url = "github:arkenfox/user.js";
       flake = false;
     };
+    # TODO replace as soon as the PR is merged 
+    "105.0" = {
+      type = "github";
+      owner = "arkenfox";
+      repo = "user.js";
+      ref = "refs/pull/1541/head";
+      flake = false;
+    };
     "104.0" = {
       url = "github:arkenfox/user.js/104.0";
       flake = false;
