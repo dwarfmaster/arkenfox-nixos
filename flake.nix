4ca08377055522b4219473130bfb48ac01516443
DwarfMaster <luc@dwarfmaster.net>
2022-09-29 23:42:08 +0200

    Refactor inputs

--- flake.nix
+++ flake.nix
@@ -15,54 +15,18 @@
       ref = "refs/pull/1541/head";
       flake = false;
     };
-    "104.0" = {
-      url = "github:arkenfox/user.js/104.0";
-      flake = false;
-    };
-    "103.0" = {
-      url = "github:arkenfox/user.js/103.0";
-      flake = false;
-    };
-    "102.0" = {
-      url = "github:arkenfox/user.js/102.0";
-      flake = false;
-    };
-    "101.0" = {
-      url = "github:arkenfox/user.js/101.0";
-      flake = false;
-    };
-    "100.0" = {
-      url = "github:arkenfox/user.js/100.0";
-      flake = false;
-    };
-    "99.0" = {
-      url = "github:arkenfox/user.js/99.0";
-      flake = false;
-    };
-    "98.0" = {
-      url = "github:arkenfox/user.js/98.0";
-      flake = false;
-    };
-    "97.0" = {
-      url = "github:arkenfox/user.js/97.0";
-      flake = false;
-    };
-    "96.0" = {
-      url = "github:arkenfox/user.js/96.0";
-      flake = false;
-    };
-    "95.0" = {
-      url = "github:arkenfox/user.js/95.0";
-      flake = false;
-    };
-    "94.0" = {
-      url = "github:arkenfox/user.js/94.0";
-      flake = false;
-    };
-    "93.0" = {
-      url = "github:arkenfox/user.js/93.0";
-      flake = false;
-    };
+    "104.0" = { url = "github:arkenfox/user.js/104.0"; flake = false; };
+    "103.0" = { url = "github:arkenfox/user.js/103.0"; flake = false; };
+    "102.0" = { url = "github:arkenfox/user.js/102.0"; flake = false; };
+    "101.0" = { url = "github:arkenfox/user.js/101.0"; flake = false; };
+    "100.0" = { url = "github:arkenfox/user.js/100.0"; flake = false; };
+    "99.0"  = { url = "github:arkenfox/user.js/99.0";  flake = false; };
+    "98.0"  = { url = "github:arkenfox/user.js/98.0";  flake = false; };
+    "97.0"  = { url = "github:arkenfox/user.js/97.0";  flake = false; };
+    "96.0"  = { url = "github:arkenfox/user.js/96.0";  flake = false; };
+    "95.0"  = { url = "github:arkenfox/user.js/95.0";  flake = false; };
+    "94.0"  = { url = "github:arkenfox/user.js/94.0";  flake = false; };
+    "93.0"  = { url = "github:arkenfox/user.js/93.0";  flake = false; };
   };
 
   outputs = inputs@{ self, nixpkgs, ... }:
